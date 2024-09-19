ccb64eac4730678e879d746048215dbd5acb9c1f
Philst4 <pzweckbronner@gmail.com>
2024-09-18 23:14:17 -0400

    removed unnecessary files

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-# Define paths
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-RAW_DIR = os.path.join(ROOT_DIR, 'data', 'raw')
-CLEAN_DIR = os.path.join(ROOT_DIR, 'data', 'clean')
-MODEL_DIR = os.path.join(ROOT_DIR, 'models')
-
