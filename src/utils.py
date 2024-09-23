cb4288ebdea9efdbd39e0cb196ba15ef53449273
Philst4 <pzweckbronner@gmail.com>
2024-09-23 12:17:24 -0400

    added query_db to utils

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,7 @@
+import os
+
 import pandas as pd
+import sqlite3
 
 def summarize(games : pd.DataFrame, check_game_counts : bool=True) -> None:
     """Summarizes basic stats of given pd.DataFrame.
@@ -37,7 +40,35 @@ def summarize(games : pd.DataFrame, check_game_counts : bool=True) -> None:
     print(f"UNIQUE ID COUNTS PER GAME ID: {teams_per_game}")
 
 
+def query_db(db_path : str, query : str) -> pd.DataFrame:
+    assert(os.path.exists(db_path)), "Database not found at path"
+    conn = sqlite3.connect(db_path)
+    dataframe = pd.read_sql_query(query, conn)
+    conn.close()
+    return dataframe
+
+game_metadata = query_db("SELECT * FROM game_metadata")
+game_data = query_db("SELECT * FROM game_data")
+
+
+def normalize(games : pd.DataFrame, summary_stats : pd.DataFrame) -> pd.DataFrame:
+    return
+    normalized_games = games.copy()
+    normalized_games[numerical_cols] = normalized_games[numerical_cols].astype(float)
+    for col in numerical_cols:
+        mean_col, std_col = summary_cols_map[col]
+        for season in list(summary_stats['SEASON_ID']):
+            season_values = cleaned_games.loc[cleaned_games['SEASON_ID'] == season, col]
+            season_mean = summary_stats.loc[summary_stats['SEASON_ID'] == season, mean_col].values
+            season_std = summary_stats.loc[summary_stats['SEASON_ID'] == season, std_col].values
+            normalized_season_values = (season_values - season_mean) / season_std
+            normalized_games.loc[normalized_games['SEASON_ID'] == season, col] = normalized_season_values
+
+    normalized_games = normalized_games.dropna()
+
+
 def get_rolling(games : pd.DataFrame, window_size : int=0) -> None:
+    return
     # Step 1: Sort by 'SEASON_ID', 'TEAM_ID_for', and 'GAME_DATE', groupby 'SEASON_ID', 'NEW_TEAM_ID_for'
     games = games.sort_values(by=['SEASON_ID', 'NEW_TEAM_ID_for', 'GAME_DATE'])
     team_groups = games.groupby(['SEASON_ID', 'NEW_TEAM_ID_for'])
